e4b75849c7e9977e3e80efcaff715f00a676f68a
Cimitra Software GitHub Repository <55113746+cimitrasoftware@users.noreply.github.com>
2021-11-06 18:11:17 -0600

    Add files via upload

---
+++ csvs/export.ps1
@@ -0,0 +1,18 @@
+﻿Param(
+[string] $OU,
+[string] $CsvFile
+)
+if($OU.Length -lt 5){
+    $TheOU = 'OU=ADMINISTRATION,OU=USERS,OU=KCC,OU=DEMOSYSTEM,DC=cimitrademo,DC=com'
+}else{
+    $TheOU = $OU
+}
+
+if($CsvFile.Length -lt 5){
+    $TheCsvFile = 'C:\temp\export.csv'
+}else{
+    $TheCsvFile = $CsvFile
+}
+
+
+Get-ADUser -Filter * -SearchBase $TheOU -Properties CanonicalName,CN,DisplayName,GivenName,Name,Surname | Export-Csv $TheCsvFile
